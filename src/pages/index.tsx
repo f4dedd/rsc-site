c424024a2b7f87bc2a9762204bd822ac2ae5c901
cake <164044366+cakecable@users.noreply.github.com>
2025-09-24 13:06:31 -0400

    Add visible content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return <>
     <div id="about" className="content-section">
         <img src={catIconUrl} alt="ppcat" className="main-icon"></img>
+        <div className="subtitle">
+          <h1 className="text-4xl font-bold mb-4">Really Silly Club</h1>
+          <p className="text-lg text-gray-300">Welcome to our community!</p>
+        </div>
     </div>
   </>
 }
